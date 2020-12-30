f1bce80366011393adf12c18be615de8d3e12adb
Semih <50872469+Semcester@users.noreply.github.com>
2020-12-30 21:12:58 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const countdown = document.querySelector('.countdown');
+
+const launchDate = new Date("Dec 31, 2021 13:00:00").getTime();
+
+const intvl = setInterval(()=>{
+
+    const now = new Date().getTime();
+
+    const distance = launchDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 *24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 *24)) / (1000 * 60 * 60));
+    const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const sec = Math.floor((distance % (1000 * 60 )) / 1000 );
+
+    countdown.innerHTML = `
+        <div>${days}<span>Days</span></div>
+        <div>${hours}<span>Hours</span></div>
+        <div>${mins}<span>Minutes</span></div>
+        <div>${sec}<span>Seconds</span></div>
+    `;
+    if(distance < 0) {
+        clearInterval(intvl);
+        countdown.style.color = '#17a2b8';
+        countdown.innerHTML ="Merry Christmas"
+    }
+
+},1000);
